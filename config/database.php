d5c5fdc5a118098667d2abc4e2fff5c73499689f
Widarka <dwidarma601@gmail.com>
2018-11-07 22:53:13 +0800

    tugas

--- config/database.php
+++ config/database.php
@@ -42,11 +42,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'us-cdbr-gcp-east-01.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'gcp_065090c37b331f53e48a'),
+            'username' => env('DB_USERNAME', 'b81ef94c09a031'),
+            'password' => env('DB_PASSWORD', 'bd4ba5a7'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
